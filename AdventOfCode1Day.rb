d39888bddbb36111f6b91945b9d4852e10a0704a
BrenoMontanha <breno.montanha@estudante.ufjf.br>
2023-12-02 08:14:35 -0300

    First Day of Advent Of Code

---
+++ AdventOfCode1Day.rb
@@ -0,0 +1,39 @@
+def number?(char)
+  !!(char =~ /[0-9]/)
+end
+
+def tratamento(str)
+    str.gsub!("one", "o1e")
+    str.gsub!("two", "t2o")
+    str.gsub!("three", "t3e")
+    str.gsub!("four", "4")
+    str.gsub!("five", "5e")
+    str.gsub!("six", "6")
+    str.gsub!("seven", "7n")
+    str.gsub!("eight", "e8t")
+    str.gsub!("nine", "n9e")
+    str.gsub!(/\D/, "")
+end
+
+soma = 0
+File.foreach("AdventOfCode1DayInput.txt") {
+    |line|
+    puts line
+    tratamento(line)
+    puts line
+    linha = line.chars
+    first = nil
+    last = nil
+    linha.each do |letra|
+        if(number?(letra))
+            last = letra
+            if(first.nil?)
+                first = letra;
+            end
+        end
+    end
+    puts first + last
+    soma += (first + last).to_i
+}
+
+puts soma
